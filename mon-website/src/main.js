13a439a935027e256f69fd9978339253b3efa560
TIAMIYU <66704012+Master250@users.noreply.github.com>
2021-07-05 02:48:20 +0200

    add router

--- mon-website/src/main.js
+++ mon-website/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(router).use(store)
 app.use(vuetify)
 
 app.mount('#app')
