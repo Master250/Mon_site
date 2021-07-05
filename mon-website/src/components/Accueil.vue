5c763c3f7faf878ec089b58df85c0a99231409dd
TIAMIYU <66704012+Master250@users.noreply.github.com>
2021-07-05 03:28:28 +0200

    ../

---
+++ mon-website/src/components/Accueil.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app id="accueil" class="text-center">
+        <top-header/>
+        <h1 >Bienvevue chez IT-Master reset</h1>       
+        
+        <router-view></router-view>
+         
+    </v-app>
+</template>
+<script>
+import Header from "./Accueil/Header"
+
+export default {
+    name: "Accueil",
+    data(){
+        return{
+            
+        }
+    },
+    components: {
+        "top-header": Header,
+    },
+}
+</script>
